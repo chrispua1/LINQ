686c0da8cd2af9b0d7cc81d2da458a5f57f3ed91
chrispua1 <112981779+chrispua1@users.noreply.github.com>
2024-11-02 20:19:04 -0400

    Update JavaScript

--- js/main.js
+++ js/main.js
@@ -1,11 +1,41 @@
-function encodeFlashcards(cards) {
+function encodeFlashcards(cards, version) {
+    switch (version) {
+        case "1":
+            return encodeFlashcardsV1(cards);
+        default:
+            return encodeFlashcardsV1;
+    }
+}
+
+function encodeFlashcardsV1(cards) {
     return btoa(Array.from(new TextEncoder().encode(cards), (byte) => String.fromCodePoint(byte),).join(""));
 }
 
-function decodeFlashcards(cards) {
+function decodeFlashcards(cards, version) {
+    switch (version) {
+        case "1":
+            return decodeFlashcardsV1(cards);
+        default:
+            return decodeFlashcardsV1(cards);
+    }
+}
+
+function decodeFlashcardsV1(cards) {
     return new TextDecoder().decode(Uint8Array.from(atob(cards), (m) => m.codePointAt(0)));
 }
 
 function cardsToObject(cards) {
-    
+    let safeString = cards
+        .replaceAll("&", "&amp;")
+        .replaceAll("<", "&lt;")
+        .replaceAll(">", "&gt;")
+        .replaceAll('"', "&quot;")
+        .replaceAll("'", "&apos;");
+    let flashcards = safeString.split(/\r\n|\r|\n/g);
+    let cards = [];
+    flashcards.forEach((element) => {
+        let theCard = element.split("\t");
+        cards.push({front: theCard[0], back: theCard[1]});
+    });
+    return cards;
 }
